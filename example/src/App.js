d15971de581428d7aff0b720aed2f529740fc04e
Josh McCall <joshmccall221@gmail.com>
2019-04-12 15:11:52 -0700

    wip

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,71 @@
 import React, { Component } from "react";
-
+import { authContext, adalApiFetch } from "./adalConfig";
 import ExampleComponent from "atomic-stories-components";
+import axios from "axios";
+// import { fetch } from "fetch";
 
 export default class App extends Component {
   render() {
+    console.log({ authContext });
     return (
       <div>
         <ExampleComponent text="Modern React component module" />
+        <div
+          onClick={() => {
+            const options = {
+              method: "GET", // *GET, POST, PUT, DELETE, etc.
+              // mode: "no-cors", // no-cors, cors, *same-origin
+              // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+              // credentials: "include", // include, *same-origin, omit
+              headers: {
+                "Content-Type": "application/json" // "Content-Type": "application/x-www-form-urlencoded",
+                // Authorization: `Bearer ${"token"}`
+              },
+              url:
+                "https://cors-anywhere.herokuapp.com/fyc-dev.azurewebsites.net/api/search/users/josh"
+            };
+            adalApiFetch(
+              axios,
+              "https://cors-anywhere.herokuapp.com/fyc-dev.azurewebsites.net/api/search/users/josh",
+              // "https://fyc-dev.azurewebsites.net/api/search/users/josh",
+              options
+            )
+              //   .then(function(response) {
+              //     console.log(response);
+              //   })
+              //   .catch(function(error) {
+              //     console.log(error);
+              //   });
+              // axios
+              //   .get(
+              //     "https://cors-anywhere.herokuapp.com/fyc-dev.azurewebsites.net/api/search/users/josh",
+              //     // "https://fyc-dev.azurewebsites.net/api/search/users/j",
+              //     options
+              //   )
+              // async populateWeatherData() {
+              //   const token = await authService.getAccessToken(); const response =
+              //     await
+              //   fetch(
+              //     'api/SampleData/WeatherForecasts',
+              //     { headers: !token ? {} : { 'Authorization': `Bearer ${token}` } }
+              //   )
+              // }
+
+              // fetch(
+              //   "https://fyc-dev.azurewebsites.net/api/search/users/josh",
+              //   options
+              // )
+              .then(function(response) {
+                console.log(response);
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+            console.log("click div");
+          }}
+        >
+          Click me!!
+        </div>
       </div>
     );
   }
--- example/src/adalConfig.js
+++ example/src/adalConfig.js
@@ -2,18 +2,24 @@ import {
   AuthenticationContext,
   adalFetch,
   withAdalLogin
-} from 'react-adal';
+} from "react-adal";
 
-// export const adalConfig = {
-//   tenant: '',
-//   clientId: '',
-//   endpoints: {},
-//   cacheLocation: 'localStorage',
-// };
+export const adalConfig = {
+  tenant: "72f988bf-86f1-41af-91ab-2d7cd011db47",
+  clientId: "4595abb4-08dc-4115-ab0a-46965d334a57",
+  endpoints: {
+    api: 'https://microsoft.onmicrosoft.com/bf9bd303-6141-4d3b-92f5-652600d47043'
+    // api: '4595abb4-08dc-4115-ab0a-46965d334a57'
+  },
+  cacheLocation: "localStorage"
+};
 
 export const authContext = new AuthenticationContext(adalConfig);
 
 export const adalApiFetch = (fetch, url, options) =>
   adalFetch(authContext, adalConfig.endpoints.api, fetch, url, options);
 
-export const withAdalLoginApi = withAdalLogin(authContext, adalConfig.endpoints.api);
+export const withAdalLoginApi = withAdalLogin(
+  authContext,
+  adalConfig.endpoints.api
+);
